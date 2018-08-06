0acc55910eba704debb6f769a0b86ef43b2ceb31
Mark <markeasting@gmail.com>
2018-08-06 15:45:31 +0200

    ye #3

--- wp-content/themes/looks/template-kapsalon.php
+++ wp-content/themes/looks/template-kapsalon.php
@@ -12,4 +12,18 @@
     </div>
 </header>
 
+<article class="maxwidth">
+    <section class="prettyprint readable">
+
+        <!-- <h1><?php the_title(); ?></h1> -->
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <?php the_content();?>
+
+        <?php endwhile; endif; ?>
+
+    </section>
+</article>
+
 <?php get_footer(); ?>
