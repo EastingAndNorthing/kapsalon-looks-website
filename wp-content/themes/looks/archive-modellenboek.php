ade317d1100c60f60cfc623836486fe2d472aa81
Mark <markeasting@gmail.com>
2020-02-25 12:21:05 +0100

    Updated modellenboek header + make logo clickable

--- wp-content/themes/looks/archive-modellenboek.php
+++ wp-content/themes/looks/archive-modellenboek.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <!-- <header class="hero dark" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/bird.jpg')"> -->
-<header class="hero dark" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/bird2.jpg')">
+<header class="hero dark" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/photography/haarwerken/1.jpg')">
 
     <?php get_nav(); ?>
 
     <div class="content maxwidth">
-        <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-light.svg" alt="Kapsalon Looks">
+		<a href="/"><img class="logo" src="https://kapsalon-looks.nl/wp-content/themes/looks/img/logo-light.svg" alt="Kapsalon Looks"></a>
         <h1 class="fancy">Modellenboek</h1>
     </div>
 </header>
